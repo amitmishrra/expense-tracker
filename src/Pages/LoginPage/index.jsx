5d4f57dd0a0868a7e11610e91404103133984987
mishra4amit4@gmail.com <mishra4amit4@gmail.com>
2022-09-26 14:38:47 -0700

    login page ui fixed

--- src/Pages/LoginPage/index.jsx
+++ src/Pages/LoginPage/index.jsx
@@ -3,21 +3,21 @@ import './Style.css'
 const Login = ()=>{
     return (
     <>
-    <div className="LoginContainer pt-44 w-[90%] m-auto h-[85vh] flex flex-col justify-center items-center font-class">
-        <div className=" w-1/2 bg-orange-700 h-[65vh] rounded-[10px] flex flex-col justify-around items-center font-class">
+    <div className=" pt-44  m-auto h-[85vh] flex flex-col justify-center items-center font-class">
+        <div className="loginContainer w-[85%] md:w-1/2 bg-orange-700 h-[65vh] rounded-[20px] flex flex-col justify-around items-center font-class">
             <div className=" text-[25px] md:text-[30px] text-white ">
                 Login
             </div>
 
-            <div className="">
+            <div className="w-full flex justify-center items-center">
                 <input type="text" required placeholder='Full Name' className='loginInputs'/>
             </div>
 
-            <div className="">
+            <div className="w-full flex justify-center items-center">
             <input type="mail" required placeholder='Email' className='loginInputs'/>
             </div>
 
-            <div className="">
+            <div className="w-full flex justify-center items-center">
             <input type="tel" required placeholder='Phone Number' className='loginInputs'/>
             </div>
 
