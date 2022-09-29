a241e96d3b4ddb458a88b76579ff293faf255e76
Farhan akhtar <sheikhfarhan722@gmail.com>
2022-09-29 15:39:49 +0530

    account page refactor

--- src/Pages/Account/buttons.jsx
+++ src/Pages/Account/buttons.jsx
@@ -4,12 +4,12 @@ const upi = document.querySelector('.upi');
 const Buttons = () => {
     return (
         <>
-            <div className="text-white flex items-center md:w-[60%] w-[90%] justify-around ">
-                <button className=" rounded-[10px] py-3 px-12 bg-blue-500  " >
+            <div className="text-white flex items-center md:w-[60%] w-[90%] md:px-0 px-5 justify-around ">
+                <button className=" rounded-[10px] md:py-3 py-2 md:px-12 px-8  bg-blue-500  " >
                     Share
                 </button>
                 <div className="tooltip">
-                <button className="copy rounded-[10px] py-3 px-12 bg-blue-500 "
+                <button className="copy rounded-[10px] md:py-3 py-2 md:px-12 px-8  bg-blue-500 "
                     onClick={() => navigator.clipboard.writeText(`Example123@upi`)}
                 >
                     <span class="tooltiptext" id="myTooltip">Copy to clipboard</span>
--- src/Pages/Account/index.jsx
+++ src/Pages/Account/index.jsx
@@ -12,9 +12,9 @@ const Account = () => {
     return (
         <>
             <div className=" flex flex-col md:flex-row pt-26 w-[100vw] ">
-                <div className=" text-white flex flex-col justify-around md:mt-[10vh] items-center right-0 md:w-[50vw] md:h-[90vh] h-[100vh] ">
-                    <div className=" flex flex-col justify-around items-center md:mt-3 mt-[10vh] md:rounded-[80px] rounded-[20px] p-3 md:h-[86%] h-[70%] md:w-[30vw] w-[70%] bg-amber-900 ">
-                        <img className=" rounded-[50%]  w-[180px] h-[180px] md:mt-8 mt-[5vh] bg-white" src="/" alt="" />
+                <div className=" text-white flex flex-col justify-around md:mt-[10vh] mt-0 items-center right-0 md:w-[50vw] md:h-[90vh] h-[90vh]  ">
+                    <div className="accInfo text-2xl flex flex-col justify-around items-center md:mt-11 mt-[10vh] md:rounded-[20px] rounded-[20px] p-3 md:h-[86%] h-[70%] md:w-[30vw] w-[70%]  ">
+                        <img className=" rounded-[50%]  md:w-[180px] w-[150px] md:h-[180px] h-[150px] md:mt-8 mt-[5vh] bg-white" src="/" alt="" />
                         <p>Name </p>
                         <p>Email</p>
                         <p>Phone number</p>
@@ -23,8 +23,8 @@ const Account = () => {
                         <img src="" alt="" />
                     </div>
                 </div>
-                <div className="  flex flex-col items-center justify-around md:mt-[18vh] left-0 md:w-[50vw] md:h-[80vh] h-[92vh]  ">
-                    <img className="w-[400px] h-[400px] bg-white" src="https://upiqr.in/api/qr?name=Amit&vpa=mishra4amit4@ybl" alt="" />
+                <div className="  flex flex-col items-center justify-around md:mt-[18vh] md:py-[50px] py-[80px] left-0 md:w-[50vw] md:h-[80vh] h-[90vh]  ">
+                    <img className="md:w-[300px] w-[250px] md:h-[300px] h-[250px] bg-white" src="https://upiqr.in/api/qr?name=Amit&vpa=mishra4amit4@ybl" alt="" />
                     <p id="upi" className="upi text-white text-2xl " value="Example123@upi" >Example123@upi</p>
 
                     <Buttons />
